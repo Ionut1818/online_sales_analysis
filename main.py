5083e028d4a81a3287825c841a154ba7266677ee
Padureanu Ionut-Andrei <ionutpadureanu100@gmail.com>
2025-03-05 00:11:40 +0200

    Schimbare cantitate

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ if __name__ == "__main__":
     lista = ProductManager()
     
     
-lista.add_product(Product("iPhone", 4000, 10))
-lista.add_product(Product("Casti", 400, 10))
-lista.add_product(Product("Incarcator", 120, 10))
+lista.add_product(Product("iPhone", 4000, 5))
+lista.add_product(Product("Casti", 400, 5))
+lista.add_product(Product("Incarcator", 120, 5))
 
-print("Lista produselor:")
-lista.display_products()
 
-print(f"Valoarea inventarului: {lista.total_inventory_value()}")
